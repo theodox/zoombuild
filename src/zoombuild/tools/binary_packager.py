12216a106a9da542cdaeaddc7444397dfdbf00f3
Steve Theodore <theodox@gmail.com>
2025-04-19 17:24:46 -0700

    move unpacker text into its own file

--- src/zoombuild/tools/binary_packager.py
+++ src/zoombuild/tools/binary_packager.py
@@ -11,10 +11,13 @@ import configparser
 import click
 from io import StringIO
 from datetime import datetime
+import importlib.resources as resources
+
 from .project_info import PyProject
 
-__builder__ = "zoombuild"
-__version__ = "0.1.3"
+
+__version__ = "0.1.4"
+
 METADATA_FILE = "environment.ini"
 DEPLOY_KEY = "deploy"
 FOLDER_KEY = "folder"
@@ -28,45 +31,21 @@ formatter = logging.Formatter("{message}", style="{")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
+
 # this script is included
 # as the __main__ of the
 # binary zip
-unzip_main = f"""
-import os
-import zipfile
-import struct
-import shutil
-import sys
-import configparser
-
-zip = os.path.dirname(__file__)
-cfg = configparser.ConfigParser()
-with zipfile.ZipFile(zip, "r") as archive:
-    with archive.open('{METADATA_FILE}', 'r') as handle:
-        data = handle.read().decode('utf-8')
-        cfg.read_string(data)
-    deploy_path = cfg['{DEPLOY_KEY}']['{FOLDER_KEY}']
-    checksum = cfg['{DEPLOY_KEY}']['{CHECKSUM_KEY}']
-        
-if not os.path.isdir(deploy_path):
-    print ("fresh deployment, unpacking into " + deploy_path)
-    shutil.unpack_archive(zip, deploy_path)
-    sys.exit(0)
-else:
-    parser = configparser.ConfigParser()
-    checksum_file = os.path.join(deploy_path, '{METADATA_FILE}')
-    parser.read(checksum_file)
-    saved_checksum = parser['{DEPLOY_KEY}']['{CHECKSUM_KEY}']
-    print("zip checksum", checksum, "disk checksum", saved_checksum)
-    if (checksum == saved_checksum):
-        print ("dependencies unchanged")
-        sys.exit(0)
-    else:
-        print ("dependencies have changed, updating deployment")
-        shutil.rmtree(deploy_path)
-        shutil.unpack_archive(zip, extract_dir = deploy_path)
-        sys.exit(0)
-"""
+def generate_unzip_text():
+    unzip_text = resources.files(__name__).joinpath("unpacker_script.txt").read_text()
+    
+    readable_version, *_ = sys.version.split(" ")
+    return unzip_text.format(
+        METADATA_FILE=METADATA_FILE,
+        VERSION=readable_version,
+        DEPLOY_KEY=DEPLOY_KEY,
+        FOLDER_KEY=FOLDER_KEY,
+        CHECKSUM_KEY=CHECKSUM_KEY,
+    )
 
 
 def validate_zip(checksum, zip):
@@ -76,7 +55,6 @@ def validate_zip(checksum, zip):
             data = cs.read().decode("utf-8")
             parser.read_string(data)
             zip_checksum = parser[DEPLOY_KEY][CHECKSUM_KEY]
-
             archive_version = parser["metadata"]["version"]
         return zip_checksum == checksum and archive_version == str(__version__)
 
@@ -92,7 +70,7 @@ def collect_requirements(project: PyProject):
     return result, checksum
 
 
-def precompile_bytecode(venv_site_packages):
+def _precompile_bytecode(venv_site_packages):
     if compileall.compile_path(venv_site_packages, optimize=2, quiet=2):
         logger.info("compiled bytecode")
     else:
@@ -100,14 +78,14 @@ def precompile_bytecode(venv_site_packages):
         sys.exit(99)
 
 
-def precompute_file_count(directory):
+def _precompute_file_count(directory):
     total_items = 0
     for _, dirs, files in os.walk(directory):
         total_items += len(files)
     return total_items
 
 
-def generate_ini(project, output, deploy_folder, checksum):
+def _generate_ini(project, output, deploy_folder, checksum):
     """
     Returns a string in INI format with metadata about this build.
 
@@ -121,12 +99,13 @@ def generate_ini(project, output, deploy_folder, checksum):
         "name": project.name,
         "version": project.version,
     }
-    cfg["metadata"] = {
+    cfg["build"] = {
         "created": datetime.now(),
         "machine": os.getenv("COMPUTERNAME", "unknown"),
         "user": os.getenv("USERNAME", "unknown"),
         "version": __version__,
-        "tool": __builder__,
+        "tool": __name__,
+        "python": sys.version
     }
     cfg[DEPLOY_KEY] = {
         FOLDER_KEY: deploy_folder,
@@ -160,11 +139,11 @@ def archive_venv(project: PyProject, output=None, deploy_folder="deploy"):
             logger.warning("dependencies or version have changed")
             os.remove(target_zip)
 
-    precompile_bytecode(venv_site_packages)
+    _precompile_bytecode(venv_site_packages)
 
     try:
         with zipfile.ZipFile(target_zip, "w") as archive:
-            file_total = precompute_file_count(venv_site_packages)
+            file_total = _precompute_file_count(venv_site_packages)
             with tqdm.tqdm(total=file_total, desc="copying", unit=" files") as progress:
                 for root, _, files in os.walk(venv_site_packages):
                     if root == str(venv_site_packages):
@@ -179,14 +158,15 @@ def archive_venv(project: PyProject, output=None, deploy_folder="deploy"):
                         logger.debug(f)
 
                 logger.debug("adding unzipper")
-                archive.writestr("__main__.py", unzip_main)
+                main_script = generate_unzip_text()
+                archive.writestr("__main__.py", main_script)
                 progress.update(1)
 
                 logger.debug("adding requirements")
                 archive.writestr("requirements.txt", requirements)
                 progress.update(1)
 
-                INI_text = generate_ini(project, output, deploy_folder, checksum)
+                INI_text = _generate_ini(project, output, deploy_folder, checksum)
                 archive.writestr(METADATA_FILE, INI_text)
                 progress.update(1)
                 progress.update(1)
